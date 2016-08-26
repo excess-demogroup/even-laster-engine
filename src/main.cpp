fa1d7c01c5be5ce14f30b47aeb4f545a09e97588
Erik Faye-Lund <kusmabite@gmail.com>
2016-08-26 15:33:53 +0200

    hack a swapchain-class

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,147 @@
 
 using namespace vulkan;
 
+class SwapChain
+{
+public:
+	SwapChain(VkSurfaceKHR surface, int width, int height) :
+		swapChain(VK_NULL_HANDLE)
+	{
+		VkBool32 surfaceSupported = VK_FALSE;
+		VkResult err = vkGetPhysicalDeviceSurfaceSupportKHR(physicalDevice, graphicsQueueIndex, surface, &surfaceSupported);
+		assert(err == VK_SUCCESS);
+		assert(surfaceSupported == VK_TRUE);
+
+		VkSurfaceCapabilitiesKHR surfCaps;
+		err = vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice, surface, &surfCaps);
+		assert(err == VK_SUCCESS);
+
+		uint32_t surfaceFormatCount = 0;
+		err = vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &surfaceFormatCount, nullptr);
+		assert(err == VK_SUCCESS);
+		assert(surfaceFormatCount > 0);
+		std::vector<VkSurfaceFormatKHR> surfaceFormats;
+		surfaceFormats.resize(surfaceFormatCount);
+		err = vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &surfaceFormatCount, surfaceFormats.data());
+		assert(err == VK_SUCCESS);
+
+		// find sRGB color format
+		surfaceFormat = surfaceFormats[0];
+		for (size_t i = 1; i < surfaceFormatCount; ++i) {
+			if (surfaceFormats[i].colorSpace == VK_COLORSPACE_SRGB_NONLINEAR_KHR) {
+				surfaceFormat = surfaceFormats[i];
+				break;
+			}
+		}
+		assert(surfaceFormat.colorSpace == VK_COLORSPACE_SRGB_NONLINEAR_KHR);
+
+		uint32_t presentModeCount = 0;
+		err = vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, surface, &presentModeCount, nullptr);
+		assert(err == VK_SUCCESS);
+		assert(presentModeCount > 0);
+		auto presentModes = new VkPresentModeKHR[presentModeCount];
+		err = vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, surface, &presentModeCount, presentModes);
+		assert(err == VK_SUCCESS);
+
+		VkSwapchainCreateInfoKHR swapchainCreateInfo = {};
+		swapchainCreateInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+		swapchainCreateInfo.surface = surface;
+		swapchainCreateInfo.minImageCount = min(surfCaps.minImageCount + 1, surfCaps.maxImageCount);
+		swapchainCreateInfo.imageFormat = surfaceFormat.format;
+		swapchainCreateInfo.imageColorSpace = surfaceFormat.colorSpace;
+		swapchainCreateInfo.imageExtent = { width, height };
+		swapchainCreateInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+		swapchainCreateInfo.preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
+		swapchainCreateInfo.imageArrayLayers = 1;
+		swapchainCreateInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+		swapchainCreateInfo.queueFamilyIndexCount = 0;
+		swapchainCreateInfo.pQueueFamilyIndices = nullptr;
+
+		swapchainCreateInfo.presentMode = presentModes[0];
+		for (uint32_t i = 0; i < presentModeCount; ++i)
+			if (presentModes[i] == VK_PRESENT_MODE_MAILBOX_KHR)
+				swapchainCreateInfo.presentMode = presentModes[i];
+
+		swapchainCreateInfo.oldSwapchain = VK_NULL_HANDLE;
+		swapchainCreateInfo.clipped = true;
+		swapchainCreateInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+
+		err = vkCreateSwapchainKHR(device, &swapchainCreateInfo, nullptr, &swapChain);
+		assert(err == VK_SUCCESS);
+		assert(swapChain != VK_NULL_HANDLE);
+
+		uint32_t imageCount;
+		err = vkGetSwapchainImagesKHR(device, swapChain, &imageCount, nullptr);
+		assert(err == VK_SUCCESS);
+		assert(imageCount > 0);
+
+		// Get the swap chain images
+		images.resize(imageCount);
+		err = vkGetSwapchainImagesKHR(device, swapChain, &imageCount, images.data());
+		assert(err == VK_SUCCESS);
+
+		imageViews.resize(imageCount);
+		for (uint32_t i = 0; i < imageCount; i++) {
+			VkImageViewCreateInfo imageView = {};
+			imageView.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			imageView.format = surfaceFormat.format;
+			imageView.components = {
+				VK_COMPONENT_SWIZZLE_R,
+				VK_COMPONENT_SWIZZLE_G,
+				VK_COMPONENT_SWIZZLE_B,
+				VK_COMPONENT_SWIZZLE_A
+			};
+			imageView.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			imageView.subresourceRange.baseMipLevel = 0;
+			imageView.subresourceRange.levelCount = 1;
+			imageView.subresourceRange.baseArrayLayer = 0;
+			imageView.subresourceRange.layerCount = 1;
+			imageView.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			imageView.image = images[i];
+
+			err = vkCreateImageView(device, &imageView, nullptr, &imageViews[i]);
+			assert(err == VK_SUCCESS);
+		}
+	}
+
+	const std::vector<VkImageView> &getImageViews() const
+	{
+		return imageViews;
+	}
+
+	const VkSurfaceFormatKHR &getSurfaceFormat() const
+	{
+		return surfaceFormat;
+	}
+
+	uint32_t aquireNextImage(VkSemaphore presentCompleteSemaphore)
+	{
+		uint32_t currentSwapImage;
+		VkResult err = vkAcquireNextImageKHR(device, swapChain, UINT64_MAX, presentCompleteSemaphore, VK_NULL_HANDLE, &currentSwapImage);
+		assert(err == VK_SUCCESS);
+
+		return currentSwapImage;
+	}
+
+	void queuePresent(uint32_t currentSwapImage)
+	{
+		VkPresentInfoKHR presentInfo = {};
+		presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+		presentInfo.swapchainCount = 1;
+		presentInfo.pSwapchains = &swapChain;
+		presentInfo.pImageIndices = &currentSwapImage;
+		VkResult err = vkQueuePresentKHR(graphicsQueue, &presentInfo);
+		assert(err == VK_SUCCESS);
+	}
+
+private:
+	VkSurfaceFormatKHR surfaceFormat;
+	VkSwapchainKHR swapChain;
+	uint32_t imageCount;
+	std::vector<VkImage> images;
+	std::vector<VkImageView> imageViews;
+};
+
 VkShaderModule loadShaderModule(const char *path, VkDevice device, VkShaderStageFlagBits stage)
 {
 	MemoryMappedFile shaderCode(path);
@@ -185,104 +326,11 @@ int main(int argc, char *argv[])
 		if (err)
 			throw std::exception("glfwCreateWindowSurface failed!");
 
-		VkBool32 surfaceSupported = VK_FALSE;
-		err = vkGetPhysicalDeviceSurfaceSupportKHR(physicalDevice, graphicsQueueIndex, surface, &surfaceSupported);
-		assert(err == VK_SUCCESS);
-		assert(surfaceSupported == VK_TRUE);
-
-		VkSurfaceCapabilitiesKHR surfCaps;
-		err = vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice, surface, &surfCaps);
-		assert(err == VK_SUCCESS);
-
-		uint32_t surfaceFormatCount = 0;
-		err = vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &surfaceFormatCount, nullptr);
-		assert(err == VK_SUCCESS);
-		assert(surfaceFormatCount > 0);
-		auto surfaceFormats = new VkSurfaceFormatKHR[surfaceFormatCount];
-		err = vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &surfaceFormatCount, surfaceFormats);
-		assert(err == VK_SUCCESS);
-
-		// find sRGB color format
-		VkSurfaceFormatKHR surfaceFormat = surfaceFormats[0];
-		for (size_t i = 1; i < surfaceFormatCount; ++i) {
-			if (surfaceFormats[i].colorSpace == VK_COLORSPACE_SRGB_NONLINEAR_KHR) {
-				surfaceFormat = surfaceFormats[i];
-				break;
-			}
-		}
-		assert(surfaceFormat.colorSpace == VK_COLORSPACE_SRGB_NONLINEAR_KHR);
-
-		uint32_t presentModeCount = 0;
-		err = vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, surface, &presentModeCount, nullptr);
-		assert(err == VK_SUCCESS);
-		assert(presentModeCount > 0);
-		auto presentModes = new VkPresentModeKHR[presentModeCount];
-		err = vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, surface, &presentModeCount, presentModes);
-		assert(err == VK_SUCCESS);
-
-		VkSwapchainCreateInfoKHR swapchainCreateInfo = {};
-		swapchainCreateInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
-		swapchainCreateInfo.surface = surface;
-		swapchainCreateInfo.minImageCount = min(surfCaps.minImageCount + 1, surfCaps.maxImageCount);
-		swapchainCreateInfo.imageFormat = surfaceFormat.format;
-		swapchainCreateInfo.imageColorSpace = surfaceFormat.colorSpace;
-		swapchainCreateInfo.imageExtent = { width, height };
-		swapchainCreateInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
-		swapchainCreateInfo.preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
-		swapchainCreateInfo.imageArrayLayers = 1;
-		swapchainCreateInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
-		swapchainCreateInfo.queueFamilyIndexCount = 0;
-		swapchainCreateInfo.pQueueFamilyIndices = nullptr;
-
-		swapchainCreateInfo.presentMode = presentModes[0];
-		for (uint32_t i = 0; i < presentModeCount; ++i)
-			if (presentModes[i] == VK_PRESENT_MODE_MAILBOX_KHR)
-				swapchainCreateInfo.presentMode = presentModes[i];
-
-		swapchainCreateInfo.oldSwapchain = VK_NULL_HANDLE;
-		swapchainCreateInfo.clipped = true;
-		swapchainCreateInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
-
-		VkSwapchainKHR swapChain = VK_NULL_HANDLE;
-		err = vkCreateSwapchainKHR(device, &swapchainCreateInfo, nullptr, &swapChain);
-		assert(err == VK_SUCCESS);
-
-		uint32_t imageCount;
-		err = vkGetSwapchainImagesKHR(device, swapChain, &imageCount, nullptr);
-		assert(err == VK_SUCCESS);
-		assert(imageCount > 0);
-
-		// Get the swap chain images
-		auto images = new VkImage[imageCount];
-		auto imageViews = new VkImageView[imageCount];
-		err = vkGetSwapchainImagesKHR(device, swapChain, &imageCount, images);
-		assert(err == VK_SUCCESS);
-
-		for (uint32_t i = 0; i < imageCount; i++) {
-			VkImageViewCreateInfo colorAttachmentView = {};
-			colorAttachmentView.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-			colorAttachmentView.format = surfaceFormat.format;
-			colorAttachmentView.components = {
-				VK_COMPONENT_SWIZZLE_R,
-				VK_COMPONENT_SWIZZLE_G,
-				VK_COMPONENT_SWIZZLE_B,
-				VK_COMPONENT_SWIZZLE_A
-			};
-			colorAttachmentView.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-			colorAttachmentView.subresourceRange.baseMipLevel = 0;
-			colorAttachmentView.subresourceRange.levelCount = 1;
-			colorAttachmentView.subresourceRange.baseArrayLayer = 0;
-			colorAttachmentView.subresourceRange.layerCount = 1;
-			colorAttachmentView.viewType = VK_IMAGE_VIEW_TYPE_2D;
-			colorAttachmentView.image = images[i];
-
-			err = vkCreateImageView(device, &colorAttachmentView, nullptr, &imageViews[i]);
-			assert(err == VK_SUCCESS);
-		}
+		auto swapChain = SwapChain(surface, width, height);
 
 		VkAttachmentDescription attachments[1];
 		attachments[0].flags = 0;
-		attachments[0].format = surfaceFormat.format;
+		attachments[0].format = swapChain.getSurfaceFormat().format;
 		attachments[0].samples = VK_SAMPLE_COUNT_1_BIT;
 		attachments[0].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
 		attachments[0].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
@@ -321,8 +369,9 @@ int main(int argc, char *argv[])
 		framebufferCreateInfo.height = height;
 		framebufferCreateInfo.layers = 1;
 
-		auto framebuffers = new VkFramebuffer[imageCount];
-		for (uint32_t i = 0; i < imageCount; i++) {
+		auto imageViews = swapChain.getImageViews();
+		auto framebuffers = new VkFramebuffer[imageViews.size()];
+		for (uint32_t i = 0; i < imageViews.size(); i++) {
 			framebufferAttachments[0] = imageViews[i];
 			VkResult err = vkCreateFramebuffer(device, &framebufferCreateInfo, nullptr, &framebuffers[i]);
 			assert(err == VK_SUCCESS);
@@ -337,7 +386,7 @@ int main(int argc, char *argv[])
 		err = vkCreateCommandPool(device, &commandPoolCreateInfo, nullptr, &commandPool);
 		assert(err == VK_SUCCESS);
 
-		auto commandBuffers = allocateCommandBuffers(commandPool, imageCount);
+		auto commandBuffers = allocateCommandBuffers(commandPool, imageViews.size());
 
 		// OK, let's prepare for rendering!
 
@@ -628,9 +677,7 @@ int main(int argc, char *argv[])
 			err = vkCreateSemaphore(device, &semaphoreCreateInfo, nullptr, &presentCompleteSemaphore);
 			assert(err == VK_SUCCESS);
 
-			uint32_t currentSwapImage;
-			err = vkAcquireNextImageKHR(device, swapChain, UINT64_MAX, presentCompleteSemaphore, VK_NULL_HANDLE, &currentSwapImage);
-			assert(err == VK_SUCCESS);
+			auto currentSwapImage = swapChain.aquireNextImage(presentCompleteSemaphore);
 
 			VkCommandBuffer commandBuffer = commandBuffers[currentSwapImage];
 			VkCommandBufferBeginInfo commandBufferBeginInfo = {};
@@ -712,13 +759,7 @@ int main(int argc, char *argv[])
 			err = vkQueueSubmit(graphicsQueue, 1, &submitInfo, VK_NULL_HANDLE);
 			assert(err == VK_SUCCESS);
 
-			VkPresentInfoKHR presentInfo = {};
-			presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
-			presentInfo.swapchainCount = 1;
-			presentInfo.pSwapchains = &swapChain;
-			presentInfo.pImageIndices = &currentSwapImage;
-			err = vkQueuePresentKHR(graphicsQueue, &presentInfo);
-			assert(err == VK_SUCCESS);
+			swapChain.queuePresent(currentSwapImage);
 
 			err = vkQueueWaitIdle(graphicsQueue);
 			assert(err == VK_SUCCESS);
