727c35a9574d638dc1da0e91013c330ef2262dee
Erik Faye-Lund <kusmabite@gmail.com>
2017-04-12 22:43:26 +0200

    drop depth-candidates with stencil
    I get validation-errors on these, because the depth image is in
    undefined state. Either need to start dealing with stencil, or drop
    stencil for now. I chose the latter.

--- src/main.cpp
+++ src/main.cpp
@@ -295,11 +295,8 @@ int main(int argc, char *argv[])
 
 		std::vector<VkFormat> depthCandidates = {
 			VK_FORMAT_D32_SFLOAT,
-			VK_FORMAT_D32_SFLOAT_S8_UINT,
-			VK_FORMAT_D24_UNORM_S8_UINT,
 			VK_FORMAT_X8_D24_UNORM_PACK32,
 			VK_FORMAT_D16_UNORM,
-			VK_FORMAT_D16_UNORM_S8_UINT,
 		};
 
 		auto depthFormat = findBestFormat(depthCandidates, VK_IMAGE_TILING_OPTIMAL, VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT);
