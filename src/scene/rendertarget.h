5b865edffd0e34bfd73babb7b0d6f58e5df11124
Erik Faye-Lund <kusmabite@gmail.com>
2018-03-18 17:40:10 +0100

    add support for Texture2DArrayRenderTarget

--- src/scene/rendertarget.h
+++ src/scene/rendertarget.h
@@ -5,7 +5,7 @@
 
 class RenderTargetBase {
 protected:
-	RenderTargetBase(VkFormat format, int width, int height, int depth, int arrayLayers, VkImageUsageFlags usage, VkImageAspectFlags aspect) :
+	RenderTargetBase(VkFormat format, VkImageType imageType, VkImageViewType imageViewType, int width, int height, int depth, int arrayLayers, VkImageUsageFlags usage, VkImageAspectFlags aspect) :
 		width(width),
 		height(height),
 		depth(depth),
@@ -14,7 +14,7 @@ protected:
 		VkImageCreateInfo imageCreateInfo = {};
 		imageCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
 		imageCreateInfo.flags = 0;
-		imageCreateInfo.imageType = VK_IMAGE_TYPE_2D;
+		imageCreateInfo.imageType = imageType;
 		imageCreateInfo.format = format;
 		imageCreateInfo.extent = { (uint32_t)width, (uint32_t)height, (uint32_t)depth };
 		imageCreateInfo.mipLevels = 1;
@@ -44,7 +44,7 @@ protected:
 		subresourceRange.levelCount = 1;
 		subresourceRange.layerCount = arrayLayers;
 
-		imageView = createImageView(image, VK_IMAGE_VIEW_TYPE_2D, format, subresourceRange);
+		imageView = createImageView(image, imageViewType, format, subresourceRange);
 	}
 
 public:
@@ -58,7 +58,7 @@ public:
 	VkImage getImage() { return image; }
 	VkImageView getImageView() { return imageView; }
 
-private:
+protected:
 	int width, height, depth;
 	int arrayLayers;
 
@@ -70,7 +70,7 @@ class ColorRenderTarget : public RenderTargetBase
 {
 public:
 	ColorRenderTarget(VkFormat format, int width, int height, VkImageUsageFlags usage = 0) :
-		RenderTargetBase(format, width, height, 1, 1, usage | VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT, VK_IMAGE_ASPECT_COLOR_BIT)
+		RenderTargetBase(format, VK_IMAGE_TYPE_2D, VK_IMAGE_VIEW_TYPE_2D, width, height, 1, 1, usage | VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT, VK_IMAGE_ASPECT_COLOR_BIT)
 	{
 	}
 };
@@ -79,9 +79,36 @@ class DepthRenderTarget : public RenderTargetBase
 {
 public:
 	DepthRenderTarget(VkFormat format, int width, int height, VkImageUsageFlags usage = 0) :
-		RenderTargetBase(format, width, height, 1, 1, usage | VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT, VK_IMAGE_ASPECT_DEPTH_BIT)
+		RenderTargetBase(format, VK_IMAGE_TYPE_2D, VK_IMAGE_VIEW_TYPE_2D, width, height, 1, 1, usage | VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT, VK_IMAGE_ASPECT_DEPTH_BIT)
 	{
 	}
+};
+
+class Texture2DArrayRenderTarget : public RenderTargetBase
+{
+public:
+	Texture2DArrayRenderTarget(VkFormat format, int width, int height, int arrayLayers, VkImageUsageFlags usage = 0) :
+		RenderTargetBase(format, VK_IMAGE_TYPE_2D, VK_IMAGE_VIEW_TYPE_2D_ARRAY, width, height, 1, arrayLayers, usage | VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT, VK_IMAGE_ASPECT_COLOR_BIT)
+	{
+		arrayImageViews.reserve(arrayLayers);
+		for (int i = 0; i < arrayLayers; ++i) {
+			VkImageSubresourceRange subresourceRange;
+			subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			subresourceRange.baseMipLevel = 0;
+			subresourceRange.baseArrayLayer = i;
+			subresourceRange.levelCount = 1;
+			subresourceRange.layerCount = 1;
+			arrayImageViews.push_back(createImageView(image, VK_IMAGE_VIEW_TYPE_2D, format, subresourceRange));
+		}
+	}
+
+	const std::vector<VkImageView> &getArrayImageViews() const
+	{
+		return arrayImageViews;
+	}
+
+private:
+	std::vector<VkImageView> arrayImageViews;
 };
 
 #endif // RENDERTARGET_H
