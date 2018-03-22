302f6e59d640b95c488c86dfb6af68b2ed19c1f9
Erik Faye-Lund <kusmabite@gmail.com>
2018-03-22 12:08:26 +0100

    add missing casts

--- src/main.cpp
+++ src/main.cpp
@@ -583,8 +583,8 @@ int main(int argc, char *argv[])
 		VkPipeline computePipeline = createComputePipeline(computePipelineLayout, loadShaderModule("data/shaders/postprocess.comp.spv"));
 
 		auto computeDescriptorPool = createDescriptorPool({
-			{ VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, imageViews.size() },
-			{ VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, imageViews.size() },
+			{ VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, uint32_t(imageViews.size()) },
+			{ VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, uint32_t(imageViews.size()) },
 		}, imageViews.size());
 
 		auto computeDescriptorSetLayouts = vector<VkDescriptorSetLayout>(imageViews.size(), computeDescriptorSetLayout);
