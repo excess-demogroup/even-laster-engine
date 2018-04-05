9e2bb4a87216c718da1891a95568b26b63a8cd99
Erik Faye-Lund <kusmabite@gmail.com>
2018-04-05 12:33:05 +0200

    add ShaderProgram et al

--- src/shader.h
+++ src/shader.h
@@ -5,4 +5,95 @@
 
 VkShaderModule loadShaderModule(const char *path);
 
+class ShaderStage {
+public:
+	ShaderStage(VkShaderStageFlagBits shaderStage, VkShaderModule shaderModule) :
+		shaderStage(shaderStage),
+		shaderModule(shaderModule)
+	{
+	}
+
+	VkPipelineShaderStageCreateInfo getPipelineShaderStageCreateInfo() const
+	{
+		VkPipelineShaderStageCreateInfo ret = {};
+		ret.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+		ret.stage = shaderStage;
+		ret.module = shaderModule;
+		ret.pName = "main";
+		return ret;
+	}
+
+private:
+	VkShaderStageFlagBits shaderStage;
+	VkShaderModule shaderModule;
+};
+
+class ShaderDescriptor {
+public:
+	ShaderDescriptor(int binding, VkDescriptorType descriptorType, int count, VkShaderStageFlags stageFlags, const std::vector<VkSampler> &immutableSamplers = {}) :
+		binding(binding),
+		descriptorType(descriptorType),
+		count(count),
+		stageFlags(stageFlags),
+		immutableSamplers(immutableSamplers)
+	{
+		assert(count > 0);
+		assert(immutableSamplers.size() == 0 || immutableSamplers.size() == count);
+	}
+
+	VkDescriptorSetLayoutBinding getBinding()
+	{
+		VkDescriptorSetLayoutBinding ret;
+		ret.binding = uint32_t(binding);
+		ret.descriptorType = descriptorType;
+		ret.descriptorCount = uint32_t(count);
+		ret.stageFlags = stageFlags;
+		ret.pImmutableSamplers = immutableSamplers.size() > 0 ? immutableSamplers.data() : nullptr;
+		return ret;
+	}
+
+private:
+	int binding;
+	VkDescriptorType descriptorType;
+	int count;
+	VkShaderStageFlags stageFlags;
+	const std::vector<VkSampler> immutableSamplers;
+};
+
+
+class ShaderProgram {
+public:
+	ShaderProgram(const std::vector<ShaderStage> &stages, const std::vector<ShaderDescriptor> &descriptors, const std::vector<VkPushConstantRange> &pushConstantRanges = {}) :
+		stages(stages),
+		descriptors(descriptors)
+	{
+		std::vector<VkDescriptorSetLayoutBinding> layoutBindings;
+		layoutBindings.reserve(descriptors.size());
+		for (auto descriptor : descriptors)
+			layoutBindings.push_back(descriptor.getBinding());
+
+		descriptorSetLayout = vulkan::createDescriptorSetLayout(layoutBindings);
+		pipelineLayout = vulkan::createPipelineLayout({ descriptorSetLayout }, pushConstantRanges);
+	}
+
+	VkDescriptorSetLayout getDescriptorSetLayout() const { return descriptorSetLayout; }
+	VkPipelineLayout getPipelineLayout() const { return pipelineLayout; }
+
+	std::vector<VkPipelineShaderStageCreateInfo> getPipelineShaderStageCreateInfos() const
+	{
+		std::vector<VkPipelineShaderStageCreateInfo> ret;
+		ret.reserve(stages.size());
+		for (auto stage : stages)
+			ret.push_back(stage.getPipelineShaderStageCreateInfo());
+
+		return ret;
+	}
+
+private:
+	VkDescriptorSetLayout descriptorSetLayout;
+	VkPipelineLayout pipelineLayout;
+	const std::vector<ShaderStage> stages;
+	const std::vector<ShaderDescriptor> descriptors;
+};
+
 #endif /* SHADER_H */
